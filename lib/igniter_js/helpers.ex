95b61cbbfc0b4b634bc32bbc66be51a8d5b9754e
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-12-21 00:41:14 +0330

    Add documents of elixir part

--- lib/igniter_js/helpers.ex
+++ lib/igniter_js/helpers.ex
@@ -1,6 +1,14 @@
 defmodule IgniterJS.Helpers do
   @doc """
-  Normalize the output of the NIFs.
+  Normalize the output of the NIFs. It is a macro and returns a tuple with the first
+  element as the output, the second element as the caller function name, and the
+  third element as the status.
+
+  ```elixir
+  require IgniterJS.Helpers
+  normalize_output({:ok, :fun_atom, result}, __ENV__.function)
+  normalize_output({:error, :fun_atom, result}, __ENV__.function)
+  ```
   """
   defmacro normalize_output(output, caller_function) do
     quote do
@@ -8,6 +16,14 @@ defmodule IgniterJS.Helpers do
     end
   end
 
+  @doc """
+  Read and validate the file. It returns the file content if the file exists and the
+  extension is `.js` or `.ts`, otherwise, it returns an error tuple.
+
+  ```elixir
+  read_and_validate_file("/path/to/file.js")
+  ```
+  """
   def read_and_validate_file(file_path) do
     with true <- File.exists?(file_path),
          true <- Path.extname(file_path) in [".js", ".ts"],
@@ -19,6 +35,16 @@ defmodule IgniterJS.Helpers do
     end
   end
 
+  @doc """
+  Call the NIF function with the given file path or content and return the result.
+  It helps to change the function name as atom based on its caller function.
+
+  ```elixir
+  call_nif_fn("/path/to/file.js", __ENV__.function, fn content -> content end, :path)
+  call_nif_fn("file content", __ENV__.function, fn content -> content end)
+  call_nif_fn("file content", __ENV__.function, fn content -> content end, :content)
+  ```
+  """
   def call_nif_fn(file_path, caller_function, processing_fn, type \\ :content)
 
   def call_nif_fn(file_content, caller_function, processing_fn, :content) do
--- lib/igniter_js/parsers/javascript/parser.ex
+++ lib/igniter_js/parsers/javascript/parser.ex
@@ -1,15 +1,31 @@
 defmodule IgniterJS.Parsers.Javascript.Parser do
   alias IgniterJS.Native
-  require IgniterJS.Helpers
   import IgniterJS.Helpers, only: [call_nif_fn: 4]
 
   @doc """
-  Check if a module is imported in the given file.
+  Check if a module is imported in the given file or content or content and returns boolean.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.module_imported?(js_content, "module")
+  Parser.module_imported?(js_content, "module", :content)
+  Parser.module_imported?("/path/to/file.js", "module", :path)
+  ```
   """
   def module_imported?(file_path_or_content, module, type \\ :content) do
     elem(module_imported(file_path_or_content, module, type), 0) == :ok
   end
 
+  @doc """
+  Check if a module is imported in the given file or content or contents and return tuple.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.module_imported(js_content, "module")
+  Parser.module_imported(js_content, "module", :content)
+  Parser.module_imported("/path/to/file.js", "module", :path)
+  ```
+  """
   def module_imported(file_path_or_content, module, type \\ :content) do
     call_nif_fn(
       file_path_or_content,
@@ -22,7 +38,14 @@ defmodule IgniterJS.Parsers.Javascript.Parser do
   end
 
   @doc """
-  Insert imports to the given file.
+  Insert imports to the given file or content and returns tuple.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.insert_imports(js_content, imports_lines)
+  Parser.insert_imports(js_content, imports_lines, :content)
+  Parser.insert_imports("/path/to/file.js", imports_lines, :path)
+  ```
   """
   def insert_imports(file_path_or_content, imports_lines, type \\ :content) do
     call_nif_fn(
@@ -36,7 +59,15 @@ defmodule IgniterJS.Parsers.Javascript.Parser do
   end
 
   @doc """
-  Remove imports from the given file.
+  Remove imports from the given file or content. it accepts a single module or a list of modules.
+  It returns a tuple.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.remove_imports(js_content, "SomeModule")
+  Parser.remove_imports(js_content, ["SomeModule", "AnotherModule"], :content)
+  Parser.remove_imports("/path/to/file.js", "SomeModule", :path)
+  ```
   """
   def remove_imports(file_path_or_content, module, type \\ :content)
 
@@ -56,7 +87,28 @@ defmodule IgniterJS.Parsers.Javascript.Parser do
   end
 
   @doc """
-  Check if a LiveSocket var exists in the given file.
+  Check if a LiveSocket var exists in the given file or content and returns boolean.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.exist_live_socket?(js_content)
+  Parser.exist_live_socket?(js_content, :content)
+  Parser.exist_live_socket?("/path/to/file.js", :path)
+  ```
+  """
+  def exist_live_socket?(file_path_or_content, type \\ :content) do
+    elem(exist_live_socket(file_path_or_content, type), 0) == :ok
+  end
+
+  @doc """
+  Check if a LiveSocket var exists in the given file or content and returns tuple.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.exist_live_socket(js_content)
+  Parser.exist_live_socket(js_content, :content)
+  Parser.exist_live_socket("/path/to/file.js", :path)
+  ```
   """
   def exist_live_socket(file_path_or_content, type \\ :content) do
     call_nif_fn(
@@ -69,12 +121,17 @@ defmodule IgniterJS.Parsers.Javascript.Parser do
     )
   end
 
-  def exist_live_socket?(file_path_or_content, type \\ :content) do
-    elem(exist_live_socket(file_path_or_content, type), 0) == :ok
-  end
-
   @doc """
-  Extend the hook object in the given file.
+  Extend the hook object in the given file or content. It accepts a single object
+  or a list of objects.
+  It returns a tuple.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.extend_hook_object(js_content, "SomeObject")
+  Parser.extend_hook_object(js_content, ["SomeObject", "AnotherObject"], :content)
+  Parser.extend_hook_object("/path/to/file.js", "SomeObject", :path)
+  ```
   """
   def extend_hook_object(file_path_or_content, object_name, type \\ :content)
 
@@ -94,7 +151,16 @@ defmodule IgniterJS.Parsers.Javascript.Parser do
   end
 
   @doc """
-  Remove objects from the hooks in the given file.
+  Remove objects from the hooks in the given file or content. It accepts a single o
+  bject or a list of objects.
+  It returns a tuple.
+
+  ```elixir
+  alias IgniterJS.Parsers.Javascript.Parser
+  Parser.remove_objects_from_hooks(js_content, "SomeObject")
+  Parser.remove_objects_from_hooks(js_content, ["SomeObject", "AnotherObject"], :content)
+  Parser.remove_objects_from_hooks("/path/to/file.js", "SomeObject", :path)
+  ```
   """
   def remove_objects_from_hooks(file_path_or_content, object_name, type \\ :content)
 
