1105149bcf7fe22a50b5617bb16da33e142a3df4
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-12-21 09:37:42 +0330

    Add some init functions of mix.exs

--- mix.exs
+++ mix.exs
@@ -1,16 +1,43 @@
 defmodule IgniterJS.MixProject do
   use Mix.Project
   @version "0.0.1"
+  @source_url "https://github.com/ash-project/igniter_js"
 
   def project do
     [
       app: :igniter_js,
       version: @version,
       elixir: "~> 1.17",
+      name: "IgniterJS",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: @source_url
     ]
   end
+
+  defp description() do
+    "Codemods for js for Elixir, using rust NIFs"
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs LICENSE README*),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url,
+        "Discord" => "https://discord.gg/HTHRaaVPUc",
+        "Website" => "https://ash-hq.org",
+        "Forum" => "https://elixirforum.com/c/ash-framework-forum/",
+        "Changelog" => "#{@source_url}/blob/main/CHANGELOG.md"
+      }
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -23,7 +50,8 @@ defmodule IgniterJS.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.35.1"}
+      {:rustler, "~> 0.35.1"},
+      {:ex_doc, "~> 0.35.1", only: [:dev, :test], runtime: false}
     ]
   end
 end
